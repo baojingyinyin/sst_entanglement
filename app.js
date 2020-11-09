423567865fc29748cd43c6fae1c4c23fa1481f37
Yin Yu <yin_yu@ucsb.edu>
2020-11-08 17:12:22 -0800

    ready to deploy

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ app.get("/", function (req, res) {
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
+
+
+//create socket 
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection',newConnection);
+
+function newConnection(socket){
+          
+          console.log('new connection:'+socket.id); 
+          socket.on('mouse',mouseMsg); 
+          function mouseMsg(data){
+           socket.broadcast.emit('mouse',data);
+             console.log(data);        
+          }
+}
